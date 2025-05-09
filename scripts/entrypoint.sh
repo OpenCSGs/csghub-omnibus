1fd2aa211d73640d9ce8592468d262b645422341
mason <xiang.ma@opencsg.com>
2025-05-09 18:24:11 +0800

    Add service initialization retry mechanism

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,8 +6,22 @@
 # waiting for runit start
 sleep 2
 
+retry() {
+  local n=1 max=3 delay=5
+  while ! "$@"; do
+    ((n++))
+    if ((n > max)); then
+      log "ERRO" "The command has failed after $max attempts."
+      return 1
+    fi
+
+    log "WARN" "Command failed. Attempt $n/$max:"
+    sleep $delay
+  done
+}
+
 # Reconfigure all service enabled default
-/usr/bin/csghub-ctl reconfigure
+retry /usr/bin/csghub-ctl reconfigure
 
 # hangup
 wait
